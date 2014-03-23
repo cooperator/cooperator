dfba2d80b421ae567252606bd103293fa2884397
Erol Fornoles <erol.fornoles@gmail.com>
2014-03-23 13:38:41 +0800

    Spec for implicit success.

--- spec/success.rb
+++ spec/success.rb
@@ -1,5 +1,14 @@
 require 'cooperator'
 
+class ImplicitSuccess
+  prepend Cooperator
+
+  def perform
+    $before = true
+    $after = true
+  end
+end
+
 class ExplicitSuccess
   prepend Cooperator
 
@@ -16,6 +25,22 @@ prepare do
 end
 
 subject Cooperator
+
+scope 'implicit success' do
+  spec '.perform runs until #success! is called' do
+    ImplicitSuccess.perform
+
+    assert $before
+    assert $after
+  end
+
+  spec '.perform returns a success context' do
+    context = ImplicitSuccess.perform
+
+    assert context, :success?
+    refute context, :failure?
+  end
+end
 
 scope 'explicit success' do
   spec '.perform runs until #success! is called' do
